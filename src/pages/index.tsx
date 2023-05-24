a05047fe12504f9603a53058ebde062bf5f1b981
shimapon <160441081@ccalumni.meijo-u.ac.jp>
2023-05-24 22:31:32 +0900

    wip

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,43 +3,136 @@ import { GetServerSideProps } from "next";
 import Image from "next/image";
 import { FiRefreshCw } from "react-icons/fi";
 import { fetchServantData, ServantData } from "../../utils/api";
+import { useState } from "react";
 
 const handleReload = () => {
   window.location.reload(); // ページのリロード
 };
 
 const Index = ({ servantData }: Props) => {
+  const [isShowClass, setIsShowClass] = useState(false);
+  const [isShowNoble, setIsShowNoble] = useState(false);
+  const [isShowNobleRuby, setIsShowNobleRuby] = useState(false);
+  const [isShowNobleDetail, setIsShowNobleDetail] = useState(false);
+  const [isShowSex, setIsShowSex] = useState(false);
+  const [isShowSkillIcon, setIsShowSkillIcon] = useState(false);
+  const [isShowSkillName, setIsShowSkillName] = useState(false);
+  const [isShowAttr, setIsShowAttr] = useState(false);
+  const [isShow, setIsShow] = useState(false);
+
   return (
     <div className="p-4">
+      <div className="flex gap-2">
+        <div>
+          <input
+            type="checkbox"
+            checked={isShowClass}
+            onChange={() => setIsShowClass(!isShowClass)}
+          />
+          <label>クラス</label>
+        </div>
+        <div>
+          <input
+            type="checkbox"
+            checked={isShowNoble}
+            onChange={() => setIsShowNoble(!isShowNoble)}
+          />
+          <label>宝具名</label>
+        </div>
+        <div>
+          <input
+            type="checkbox"
+            checked={isShowNobleRuby}
+            onChange={() => setIsShowNobleRuby(!isShowNobleRuby)}
+          />
+          <label>ルビ</label>
+        </div>
+        <div>
+          <input
+            type="checkbox"
+            checked={isShowNobleDetail}
+            onChange={() => setIsShowNobleDetail(!isShowNobleDetail)}
+          />
+          <label>宝具詳細</label>
+        </div>
+        <div>
+          <input
+            type="checkbox"
+            checked={isShowSex}
+            onChange={() => setIsShowSex(!isShowSex)}
+          />
+          <label>性別</label>
+        </div>
+        <div>
+          <input
+            type="checkbox"
+            checked={isShowSkillIcon}
+            onChange={() => setIsShowSkillIcon(!isShowSkillIcon)}
+          />
+          <label>スキルアイコン</label>
+        </div>
+        <div>
+          <input
+            type="checkbox"
+            checked={isShowSkillName}
+            onChange={() => setIsShowSkillName(!isShowSkillName)}
+          />
+          <label>スキル名</label>
+        </div>
+        <div>
+          <input
+            type="checkbox"
+            checked={isShowAttr}
+            onChange={() => setIsShowAttr(!isShowAttr)}
+          />
+          <label>天地人</label>
+        </div>
+        <div>
+          <input
+            type="checkbox"
+            checked={isShow}
+            onChange={() => setIsShow(!isShow)}
+          />
+          <label>正解出す！</label>
+        </div>
+      </div>
       <h1 className="font-bold">サーヴァント５騎ランダム</h1>
       {servantData.map((servant, index) => {
         return (
-          <div key={index} className="p-12 mt-2 rounded bg-gray-700">
-            <h2 className="font-bold text-lg">{servant.name}</h2>
+          <div key={index} className="py-12 px-6 mt-2 rounded bg-gray-700">
+            {isShow && <h2 className="font-bold text-lg">{servant.name}</h2>}{" "}
             <p>レア：{servant.rarity}</p>
-            <p>クラス：{servant.className}</p>
-            <p>性別：{servant.gender}</p>
-            <p>天地人：{servant.attribute}</p>
-            <div className="flex gap-2 mt-2">
-              {servant.faces.map((face, i) => {
-                return (
-                  <Image src={face} key={i} width={120} height={120} alt="" />
-                );
-              })}
-            </div>
+            <p>クラス：{isShowClass && servant.className}</p>
+            <p>性別：{isShowSex && servant.gender}</p>
+            <p>天地人：{isShowAttr && servant.attribute}</p>
+            {isShow && (
+              <div className="flex gap-2 mt-2">
+                {servant.faces.map((face, i) => {
+                  return (
+                    <Image src={face} key={i} width={120} height={120} alt="" />
+                  );
+                })}
+              </div>
+            )}
             <div className="flex gap-2 mt-2 overflow-x-auto">
               {servant.skills.map((skill, i) => {
                 return (
-                  <div key={i} style={{ minWidth: "28%" }}>
-                    <p>{skill.name}</p>
+                  <div
+                    key={i}
+                    style={{ minWidth: "28%" }}
+                    className="p-2 bg-slate-800 rounded"
+                  >
+                    {isShowSkillName && <p>{skill.name}</p>}
                     <p className="text-sm">{skill.detail}</p>
-                    <Image
-                      src={skill.icon}
-                      key={i}
-                      width={60}
-                      height={60}
-                      alt=""
-                    />
+                    {isShowSkillIcon && (
+                      <Image
+                        src={skill.icon}
+                        key={i}
+                        width={60}
+                        height={60}
+                        alt=""
+                      />
+                    )}
                   </div>
                 );
               })}
@@ -47,10 +140,16 @@ const Index = ({ servantData }: Props) => {
             <div className="flex gap-2 mt-2 overflow-x-auto">
               {servant.noblePhantasms.map((noblePhantasm, i) => {
                 return (
-                  <div key={i} style={{ minWidth: "70%" }}>
-                    <p>{noblePhantasm.ruby}</p>
-                    <p>{noblePhantasm.name}</p>
-                    <p className="text-sm">{noblePhantasm.detail}</p>
+                  <div
+                    key={i}
+                    style={{ minWidth: "70%" }}
+                    className="p-2 bg-slate-600 rounded"
+                  >
+                    {isShowNobleRuby && <p>{noblePhantasm.ruby}</p>}
+                    {isShowNoble && <p>{noblePhantasm.name}</p>}
+                    {isShowNobleDetail && (
+                      <p className="text-sm">{noblePhantasm.detail}</p>
+                    )}
                     <p>ランク：{noblePhantasm.rank}</p>
                     <p>宝具種別：{noblePhantasm.type}</p>
                     <p>カード：{noblePhantasm.card}</p>
