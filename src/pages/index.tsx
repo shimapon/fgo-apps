427ad1d2cd80105aa9efc403aa3906fb1757ac6c
shimapon <160441081@ccalumni.meijo-u.ac.jp>
2023-06-01 01:19:50 +0900

    ランダムヒントボタン追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,25 @@ const handleReload = () => {
   window.location.reload(); // ページのリロード
 };
 
+function RandomSetButton({ stateFunctions }) {
+  const [randomIndex, setRandomIndex] = useState(null);
+
+  const clickRandomSet = () => {
+    const randomIndex = Math.floor(Math.random() * stateFunctions.length);
+    stateFunctions[randomIndex](true);
+    setRandomIndex(randomIndex);
+  };
+
+  return (
+    <div>
+      <button onClick={clickRandomSet}>ランダムに設定</button>
+      {randomIndex !== null && (
+        <p>設定された項目: {stateFunctions[randomIndex].name}</p>
+      )}
+    </div>
+  );
+}
+
 const Index = ({ servantData }: Props) => {
   const [isShowRare, setIsShowRare] = useState(false);
   const [isShowClass, setIsShowClass] = useState(false);
@@ -30,9 +49,49 @@ const Index = ({ servantData }: Props) => {
   const [isShowPassiveName, setIsShowPassiveName] = useState(false);
   const [isShowPassiveDetail, setIsShowPassiveDetail] = useState(false);
   const [isShowPassiveIcon, setIsShowPassiveIcon] = useState(true);
+  // TODO、宝具の文字列を押すとランダムに戻す。
+  const stateFunctions = [
+    setIsShowRare,
+    setIsShowClass,
+    setIsShowNoble,
+    setIsShowNobleD,
+    setIsShowNobleRank,
+    setIsShowNobleCard,
+    setIsShowNobleRuby,
+    setIsShowNobleType,
+    setIsShowNobleDetail,
+    setIsShowSex,
+    setIsShowSkillIcon,
+    setIsShowSkillIconD,
+    setIsShowSkillDetail,
+    setIsShowSkillName,
+    setIsShowSkillNameD,
+    setIsShowAttr,
+    setIsShowPassiveName,
+    setIsShowPassiveDetail,
+    setIsShowPassiveIcon,
+  ];
+
+  const [randomIndex, setRandomIndex] = useState(null);
+
+  const clickRandomSet = () => {
+    const randomIndex = Math.floor(Math.random() * stateFunctions.length);
+    stateFunctions[randomIndex](true);
+  };
 
   return (
     <div className="p-4 text-gray-100">
+      <div>
+        <button
+          className="m-2 w-full p-3 bg-slate-600 rounded"
+          onClick={clickRandomSet}
+        >
+          ランダムに公開
+        </button>
+        {randomIndex !== null && (
+          <p>設定された項目: {stateFunctions[randomIndex].name}</p>
+        )}
+      </div>
       <div className="grid grid-cols-3">
         <div>
           <input
