1c24a32482b6d57b6f41deb11d3b944470cd0a16
shimapon <160441081@ccalumni.meijo-u.ac.jp>
2023-08-04 22:38:08 +0900

    時間単位と件数表示

--- .github/scripts/calculate_lead_time.py
+++ .github/scripts/calculate_lead_time.py
@@ -17,13 +17,11 @@ for pull in pulls:
     created_at = datetime.strptime(pull['created_at'], '%Y-%m-%dT%H:%M:%SZ')
     closed_at = datetime.strptime(pull['closed_at'], '%Y-%m-%dT%H:%M:%SZ')
     lead_time = closed_at - created_at
-    lead_times.append(lead_time.total_seconds() / (60 * 60 * 24))  # 日単位に変換
+    lead_times.append(lead_time.total_seconds() / (60 * 60))  # 時間単位に変換
 
-# 平均リード時間を計算
-if lead_times:
-    average_lead_time = sum(lead_times) / len(lead_times)
-    with open('results.txt', 'w') as f:
-        f.write(f"Average lead time: {average_lead_time} days\n")
-else:
-    with open('results.txt', 'w') as f:
-        f.write("No closed PRs found.\n")
+# 平均リード時間とPRの数を計算
+average_lead_time = sum(lead_times) / len(lead_times)
+number_of_prs = len(pulls)
+
+print(f"Average lead time: {average_lead_time} hours")
+print(f"Number of PRs: {number_of_prs}")
