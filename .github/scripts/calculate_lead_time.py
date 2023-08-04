56d12711702a304571358c81c8cf683ea945ad6a
shimapon <160441081@ccalumni.meijo-u.ac.jp>
2023-08-04 23:08:43 +0900

    更新

--- .github/scripts/calculate_lead_time.py
+++ .github/scripts/calculate_lead_time.py
@@ -16,17 +16,32 @@ creator_counts = defaultdict(int)
 approver_counts = defaultdict(int)
 
 # APIからデータを取得
-response = requests.get(f"https://api.github.com/repos/{repo}/pulls?state=closed", headers=headers)
-pulls = response.json()
+page = 1
+pulls = []
+while True:
+    response = requests.get(
+        f"https://api.github.com/repos/{repo}/pulls?state=closed&page={page}&per_page=100", 
+        headers=headers
+    )
+    data = response.json()
+    if not data:
+        break
+    pulls.extend(data)
+    page += 1
 
 for pull in pulls:
     # 'base'ブランチが'develop'でなければスキップ
     if pull['base']['ref'] != 'develop':
         continue
-        
+
+    closed_at = datetime.strptime(pull['closed_at'], '%Y-%m-%dT%H:%M:%SZ')
+
+    # 過去3ヶ月以内のPRだけを考慮
+    if closed_at < datetime.now() - timedelta(days=90):
+        continue
+
     # PRのリード時間を計算
     created_at = datetime.strptime(pull['created_at'], '%Y-%m-%dT%H:%M:%SZ')
-    closed_at = datetime.strptime(pull['closed_at'], '%Y-%m-%dT%H:%M:%SZ')
     lead_time = closed_at - created_at
     lead_times.append(lead_time.total_seconds() / (60 * 60))  # 時間単位に変換
 
@@ -51,7 +66,7 @@ average_lead_time = sum(lead_times) / len(lead_times)
 # レポートを作成
 with open('results.txt', 'w') as f:
     f.write(f"Average lead time: {average_lead_time} hours\n")
-    f.write(f"Number of PRs: {len(pulls)}\n")
+    f.write(f"Number of PRs: {len(lead_times)}\n")  # ここは `pulls` の長さではなく、 `lead_times` の長さを使用
 
     f.write("\nPR creators:\n")
     for creator, count in creator_counts.items():
