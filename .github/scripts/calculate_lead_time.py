c5ab34e5c56bb45e36ef46dd40092388e4b4757d
shimapon <160441081@ccalumni.meijo-u.ac.jp>
2023-08-05 13:23:36 +0900

    週ごとも出す

--- .github/scripts/calculate_lead_time.py
+++ .github/scripts/calculate_lead_time.py
@@ -8,11 +8,22 @@ def count_hours(start, end):
     total_hours = (end - start).seconds / 3600 + (end - start).days * 24
     return total_hours
 
+def week_ending_date(date):
+    # Returns the ending date (Sunday) for the week in which the given date falls.
+    return date + timedelta(days=(6-date.weekday()))
+
 token = os.getenv('MY_GITHUB_TOKEN')
 headers = {'Authorization': f'token {token}'}
 repo = "shimapon/fgo-apps"
 
 daily_data = defaultdict(lambda: {
+    'Open to Merge': [],
+    'Open to Review Start': [],
+    'Review Start to Merge': [],
+    'Number of PRs': 0
+})
+
+weekly_data = defaultdict(lambda: {
     'Open to Merge': [],
     'Open to Review Start': [],
     'Review Start to Merge': [],
@@ -47,20 +58,30 @@ for pull in pulls:
             first_review_time = datetime.strptime(review['submitted_at'], '%Y-%m-%dT%H:%M:%SZ')
             break
     
+    # Daily Data
+    day = closed_at.date()
     if first_review_time:
-        daily_data[closed_at.date()]['Open to Review Start'].append(count_hours(created_at, first_review_time))
-        daily_data[closed_at.date()]['Review Start to Merge'].append(count_hours(first_review_time, closed_at))
-    daily_data[closed_at.date()]['Open to Merge'].append(count_hours(created_at, closed_at))
-    daily_data[closed_at.date()]['Number of PRs'] += 1
+        daily_data[day]['Open to Review Start'].append(count_hours(created_at, first_review_time))
+        daily_data[day]['Review Start to Merge'].append(count_hours(first_review_time, closed_at))
+    daily_data[day]['Open to Merge'].append(count_hours(created_at, closed_at))
+    daily_data[day]['Number of PRs'] += 1
 
-all_data = []
-for the_date, data in sorted(daily_data.items()):
-    day_string = the_date.strftime('%Y-%m-%d')
+    # Weekly Data
+    week_end = week_ending_date(closed_at.date())
+    if first_review_time:
+        weekly_data[week_end]['Open to Review Start'].append(count_hours(created_at, first_review_time))
+        weekly_data[week_end]['Review Start to Merge'].append(count_hours(first_review_time, closed_at))
+    weekly_data[week_end]['Open to Merge'].append(count_hours(created_at, closed_at))
+    weekly_data[week_end]['Number of PRs'] += 1
+
+all_daily_data = []
+for day, data in sorted(daily_data.items()):
+    day_string = day.strftime('%Y-%m-%d')
     avg_open_to_merge = sum(data['Open to Merge']) / len(data['Open to Merge']) if data['Open to Merge'] else 0
     avg_open_to_review = sum(data['Open to Review Start']) / len(data['Open to Review Start']) if data['Open to Review Start'] else 0
     avg_review_to_merge = sum(data['Review Start to Merge']) / len(data['Review Start to Merge']) if data['Review Start to Merge'] else 0
     
-    all_data.append({
+    all_daily_data.append({
         'Date': day_string,
         'Open to Merge': avg_open_to_merge,
         'Open to Review Start': avg_open_to_review,
@@ -68,6 +89,26 @@ for the_date, data in sorted(daily_data.items()):
         'Number of PRs': data['Number of PRs']
     })
 
-df = pd.DataFrame(all_data)
-with open('daily_results.html', 'w') as f:
-    f.write(df.to_html(index=False))
+all_weekly_data = []
+for week_end, data in sorted(weekly_data.items()):
+    week_string = week_end.strftime('%Y-%m-%d')
+    avg_open_to_merge = sum(data['Open to Merge']) / len(data['Open to Merge']) if data['Open to Merge'] else 0
+    avg_open_to_review = sum(data['Open to Review Start']) / len(data['Open to Review Start']) if data['Open to Review Start'] else 0
+    avg_review_to_merge = sum(data['Review Start to Merge']) / len(data['Review Start to Merge']) if data['Review Start to Merge'] else 0
+    
+    all_weekly_data.append({
+        'Week Ending': week_string,
+        'Open to Merge': avg_open_to_merge,
+        'Open to Review Start': avg_open_to_review,
+        'Review Start to Merge': avg_review_to_merge,
+        'Number of PRs': data['Number of PRs']
+    })
+
+# Writing to HTML
+df_daily = pd.DataFrame(all_daily_data)
+df_weekly = pd.DataFrame(all_weekly_data)
+with open('results.html', 'w') as f:
+    f.write("<h2>Daily Data</h2>")
+    f.write(df_daily.to_html(index=False))
+    f.write("<h2>Weekly Data</h2>")
+    f.write(df_weekly.to_html(index=False))
