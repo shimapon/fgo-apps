3297cb1da37bb2dad8fc1b16697abb0258d1e74d
shimapon <160441081@ccalumni.meijo-u.ac.jp>
2023-08-05 13:34:51 +0900

    hoge

--- .github/scripts/calculate_lead_time.py
+++ .github/scripts/calculate_lead_time.py
@@ -22,14 +22,18 @@ daily_data = defaultdict(lambda: {
     'Open to Merge': [],
     'Open to Review Start': [],
     'Review Start to Merge': [],
-    'Number of PRs': 0
+    'Number of PRs': 0,
+    'Creators': defaultdict(int),
+    'Approvers': defaultdict(int)
 })
 
 weekly_data = defaultdict(lambda: {
     'Open to Merge': [],
     'Open to Review Start': [],
     'Review Start to Merge': [],
-    'Number of PRs': 0
+    'Number of PRs': 0,
+    'Creators': defaultdict(int),
+    'Approvers': defaultdict(int)
 })
 
 page = 1
@@ -47,17 +51,20 @@ for pull in pulls:
         continue
     closed_at = datetime.strptime(pull['closed_at'], '%Y-%m-%dT%H:%M:%SZ')
     created_at = datetime.strptime(pull['created_at'], '%Y-%m-%dT%H:%M:%SZ')
+    creator = pull['user']['login']
     
     pull_number = pull['number']
     response = requests.get(f"https://api.github.com/repos/{repo}/pulls/{pull_number}/reviews", headers=headers)
     reviews = response.json()
 
     first_review_time = None
+    approver = None
     for review in reviews:
         if review['state'] in ['APPROVED', 'CHANGES_REQUESTED']:
             first_review_time = datetime.strptime(review['submitted_at'], '%Y-%m-%dT%H:%M:%SZ')
+            approver = review['user']['login']
             break
-    
+
     # Daily Data
     day = closed_at.date()
     if first_review_time:
@@ -65,6 +72,9 @@ for pull in pulls:
         daily_data[day]['Review Start to Merge'].append(count_hours(first_review_time, closed_at))
     daily_data[day]['Open to Merge'].append(count_hours(created_at, closed_at))
     daily_data[day]['Number of PRs'] += 1
+    daily_data[day]['Creators'][creator] += 1
+    if approver:
+        daily_data[day]['Approvers'][approver] += 1
 
     # Weekly Data
     week_end = week_ending_date(closed_at.date())
@@ -73,6 +83,9 @@ for pull in pulls:
         weekly_data[week_end]['Review Start to Merge'].append(count_hours(first_review_time, closed_at))
     weekly_data[week_end]['Open to Merge'].append(count_hours(created_at, closed_at))
     weekly_data[week_end]['Number of PRs'] += 1
+    weekly_data[week_end]['Creators'][creator] += 1
+    if approver:
+        weekly_data[week_end]['Approvers'][approver] += 1
 
 all_daily_data = []
 current_date = start_date
@@ -82,35 +95,42 @@ while current_date <= datetime.now().date():
     avg_open_to_merge = sum(data['Open to Merge']) / len(data['Open to Merge']) if data['Open to Merge'] else 0
     avg_open_to_review = sum(data['Open to Review Start']) / len(data['Open to Review Start']) if data['Open to Review Start'] else 0
     avg_review_to_merge = sum(data['Review Start to Merge']) / len(data['Review Start to Merge']) if data['Review Start to Merge'] else 0
-    
+
+    creators_str = "\n".join([f"{name}: {count} PRs" for name, count in data['Creators'].items()])
+    approvers_str = "\n".join([f"{name}: {count} approvals" for name, count in data['Approvers'].items()])
+
     all_daily_data.append({
         'Date': day_string,
         'Open to Merge': avg_open_to_merge,
         'Open to Review Start': avg_open_to_review,
         'Review Start to Merge': avg_review_to_merge,
-        'Number of PRs': data['Number of PRs']
+        'Number of PRs': data['Number of PRs'],
+        'PR Creators': creators_str,
+        'PR Approvers': approvers_str
     })
-
     current_date += timedelta(days=1)
 
 all_weekly_data = []
-current_week_end = week_ending_date(start_date)
-while current_week_end <= datetime.now().date():
-    data = weekly_data[current_week_end]
-    week_string = current_week_end.strftime('%Y-%m-%d')
+for week_end, data in sorted(weekly_data.items()):
+    week_start_str = (week_end - timedelta(days=6)).strftime('%Y-%m-%d')
+    week_end_str = week_end.strftime('%Y-%m-%d')
+    week_string = f"{week_start_str} to {week_end_str}"
     avg_open_to_merge = sum(data['Open to Merge']) / len(data['Open to Merge']) if data['Open to Merge'] else 0
     avg_open_to_review = sum(data['Open to Review Start']) / len(data['Open to Review Start']) if data['Open to Review Start'] else 0
     avg_review_to_merge = sum(data['Review Start to Merge']) / len(data['Review Start to Merge']) if data['Review Start to Merge'] else 0
-    
+
+    creators_str = "\n".join([f"{name}: {count} PRs" for name, count in data['Creators'].items()])
+    approvers_str = "\n".join([f"{name}: {count} approvals" for name, count in data['Approvers'].items()])
+
     all_weekly_data.append({
-        'Week Ending': week_string,
+        'Week': week_string,
         'Open to Merge': avg_open_to_merge,
         'Open to Review Start': avg_open_to_review,
         'Review Start to Merge': avg_review_to_merge,
-        'Number of PRs': data['Number of PRs']
+        'Number of PRs': data['Number of PRs'],
+        'PR Creators': creators_str,
+        'PR Approvers': approvers_str
     })
-
-    current_week_end += timedelta(days=7)
 
 # Writing to HTML
 df_daily = pd.DataFrame(all_daily_data)
